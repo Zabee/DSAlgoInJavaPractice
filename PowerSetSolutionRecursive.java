6e4ed0d8ded3a2512e21a69a10126214763a9642
Zabee <Zabee@users.noreply.github.com>
2021-11-21 12:54:39 +0530

    Create PowerSetSolutionRecursive.java

---
+++ PowerSetSolutionRecursive.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class PowerSetSolutionRecursive {
+    public static void main(String args[]) {
+        List<String> res = new ArrayList<>();
+        subset("abc","", res);
+        print(res);
+    }
+    
+    private static void print(List<String> subsets){
+        for(String subset : subsets){
+            System.out.println(subset);
+        }
+    }
+    private static void subset(String ip, String op, List<String> subsetsList/**, Set<String> withoutDup**/){
+        if(ip == null || ip.length() ==0){
+            // if(!"".equals(op)){
+                // System.out.println("{" + op + "}");    
+                subsetsList.add(op);
+            // }
+            return;
+        }
+        StringBuffer op1 = new StringBuffer(op);
+        StringBuffer op2 = new StringBuffer(op);
+        StringBuffer strBufIp = new StringBuffer(ip);
+        op1.append(strBufIp.charAt(0));
+        strBufIp.deleteCharAt(0);
+        //include
+        subset(strBufIp.toString(), op1.toString(), subsetsList);
+        //don't include
+        subset(strBufIp.toString(), op2.toString(), subsetsList);
+    }
+    
+}
