94c6c1f9945a0a9d9358b3a2edbd9d37c647dd8d
Zabee <Zabee@users.noreply.github.com>
2022-02-25 20:03:04 +0530

    Update ConsumerDemo.java

--- src/com/zabee/just/java8/practice/ConsumerDemo.java
+++ src/com/zabee/just/java8/practice/ConsumerDemo.java
@@ -1,15 +1,30 @@
 package com.zabee.just.java8.practice;
 
 import java.util.function.Consumer;
+import java.util.*;
 
 public class ConsumerDemo {
-
-	public static void main(String []args) {}
+	public static void main(String []args) {
+		Consumer<Integer> strConsumer = str -> System.out.print(str + "\t");
+		justPrint(strConsumer);
+		
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		numbers.add(6);
+		numbers.forEach(strConsumer);
+	}
 	
-	private void okay() {
-
-		Consumer<String> strConsumer = str -> System.out.println(str);
-		strConsumer.accept("Print this!!");
-	
+	private static void justPrint(Consumer<Integer> strConsumer) {
+	    System.out.print("Simply printing: ");
+		strConsumer.accept(10);
+		System.out.println();
 	}
 }
+/** Output
+	Simply printing: 10	
+	1	2	3	4	5	6
+**/
