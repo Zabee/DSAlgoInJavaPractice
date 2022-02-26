ebde2efd9aaeddd9f6d7ed18ebd8386aea4550f9
Zabee <Zabee@users.noreply.github.com>
2022-02-26 10:15:15 +0530

    Update FunctionFnInterfaceDemo.java

--- src/com/zabee/just/java8/practice/FunctionFnInterfaceDemo.java
+++ src/com/zabee/just/java8/practice/FunctionFnInterfaceDemo.java
@@ -9,18 +9,20 @@ import java.util.function.DoubleFunction;
 public class FunctionFnInterfaceDemo {
 
     public static void main(String[] args) {
+        //      ReturnType, Argument
        Function<Integer, Integer> fn = (x) -> x+10;
        int result = fn.apply(10);
        System.out.println(result);
        
+        //      ReturnType, Argument1, Argument2
        BiFunction<Integer, Integer, Integer> bfn = (x, y) -> x+y;
        result = bfn.apply(10, 20);
        System.out.println(result);
        
-       //Argument is Integer and Retrun Type is also <Integer>
-       IntFunction<Integer> ifn = (x) -> x+10;
-       result = ifn.apply(10);
-       System.out.println(result);
+       //Argument is Integer and Retrun Type is <Double>
+       IntFunction<Double> ifn = (x) -> 10.0;
+       double resultD = ifn.apply(10);
+       System.out.println(resultD);
        
        //Argument is Long and Retrun Type is <Integer>
        LongFunction<Integer> lfn = (x) -> 10;
@@ -37,7 +39,7 @@ public class FunctionFnInterfaceDemo {
 /** Output
         20
         30
-        20
+        10.0
         10
         10
 **/
