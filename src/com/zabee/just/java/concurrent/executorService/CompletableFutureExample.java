69da1ec3be6a9e334ffaa35b28f07d14525f3bac
Zabee <Zabee@users.noreply.github.com>
2022-02-27 00:08:13 +0530

    Update CompletableFutureExample.java

--- src/com/zabee/just/java/concurrent/executorService/CompletableFutureExample.java
+++ src/com/zabee/just/java/concurrent/executorService/CompletableFutureExample.java
@@ -1,4 +1,3 @@
-package com.zabee.just.java.concurrent;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
@@ -7,15 +6,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
+/**
+	CompletableFuture is used for asynchronous programming in Java.
+	Asynchronous programming is a means of writing non-blocking code
+	by running a task on a separate thread than the main application 
+	thread and notifying the main thread about its progress, 
+	completion or failure
+**/
 public class CompletableFutureExample {
 	
 	private static ExecutorService execService = null;
 
 	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+	    
 		CompletableFuture<Integer> completableFuture = calculateThisAsyncly(20);
+		
 		while (!completableFuture.isDone()) {
 			System.out.println("Cacluation is in porcess. Please wait..");
-			Thread.sleep(2000);
+			Thread.sleep(100);
 		}
 		System.out.println("The output is : " + completableFuture.get());
 		execService.shutdown();
@@ -26,17 +34,29 @@ public class CompletableFutureExample {
 
 		execService = Executors.newCachedThreadPool();
 		execService.submit(//
-				() -> {
-					try {
-						Thread.sleep(1500);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					completableFuture.complete(Stream.iterate(0, i -> ++i)//
-							.limit(argNumber)//
-							.reduce(0, Integer::sum));//
-
-				});//
+				                () -> {
+					                    try { Thread.sleep(1000);} 
+					                    catch (InterruptedException e) {e.printStackTrace();}
+					                    completableFuture.complete(Stream.iterate(0, i -> ++i)//
+						                                                 .limit(argNumber)//
+							                                             .reduce(0, Integer::sum));//
+				                        }
+				        );//
 		return completableFuture;
 	}
 }
+
+/** Output
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    Cacluation is in porcess. Please wait..
+    The output is : 190
+**/
