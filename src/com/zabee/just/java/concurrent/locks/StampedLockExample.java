a3fe0f0590a1d6d81fae09465f0bac7a2035e63a
Zabee <Zabee@users.noreply.github.com>
2022-02-27 13:26:33 +0530

    Update StampedLockExample.java

--- src/com/zabee/just/java/concurrent/locks/StampedLockExample.java
+++ src/com/zabee/just/java/concurrent/locks/StampedLockExample.java
@@ -1,5 +1,8 @@
 package com.zabee.just.java.concurrent.locks;
 
+/**
+	Refer https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/locks/StampedLock.html
+**/
 public class StampedLockExample {
 	// Similar to ReentrantReadWriteLock except the lock method returns a java long
 	// stamp value that will be used in unlock and can also be used for check if the
